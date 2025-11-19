f6fc06fe418dafebb3bc9d8547c5d1b815593124
ldc <823666076@qq.com>
2025-11-19 15:04:47 +0800

    feat:创建用户默认一个头像

--- src/modules/service/auth/auth.service.ts
+++ src/modules/service/auth/auth.service.ts
@@ -170,6 +170,8 @@ export class AuthService {
           clientName: userInfo?.nickname || `微信用户${openid.slice(-4)}`,
           clientType: ClientType.PERSONAL,
           clientStage: ClientStage.DEFAULT,
+          avatar:
+            'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0',
           createdBy: 'system',
         });
       } else {
